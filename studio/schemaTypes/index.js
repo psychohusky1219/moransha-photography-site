cb0ad68f943938b1fa1a32f101520705a0f018c9
AAA Low Voltage <rotem@aaalowvoltage.com>
2026-07-12 06:16:39 -0400

    revert from sanity

--- studio/schemaTypes/index.js
+++
@@ -1,5 +0,0 @@
-import {page} from './page.js'
-import {siteSettings} from './siteSettings.js'
-import {contactSettings} from './contactSettings.js'
-
-export const schemaTypes = [siteSettings, contactSettings, page]
